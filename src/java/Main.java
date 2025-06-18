4a083b304522a0643f2ee22145028cd2de3d8d7d
gaon <gaonto0303@naver.com>
2025-06-18 13:37:01 +0900

    commit

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,2 +1,22 @@
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        List<String> listFruits = new ArrayList<>();
+        Set<String> fruits = new HashSet<>();
+
+        listFruits.add("Apple");
+        listFruits.add("Apple");
+
+        fruits.add("Apple");
+        fruits.add("Apple");
+
+        for (String listFruit: listFruits) {
+            System.out.println(listFruit);
+        }
+
+        for (String fruit:fruits) {
+            System.out.println(fruit);
+        }
+    }
 }
